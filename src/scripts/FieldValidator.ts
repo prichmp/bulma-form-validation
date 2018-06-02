5178ea4a839ced58147cecf4a68c671c678f8df5
Matthew Prichard <green.matt@comcast.net>
2018-06-01 21:53:10 -0600

    Removed parse check from phone

--- src/scripts/FieldValidator.ts
+++ src/scripts/FieldValidator.ts
@@ -10,11 +10,11 @@ export default class FieldValidator
     };
 
     public static phone = (value: string) => {
-        return !FieldValidator.notBlank(value) || /^\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/.test(value) && parseFloat(value) != NaN;
+        return !FieldValidator.notBlank(value) || /^\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/.test(value);
     }
 
     public static number = (value: string) => {
-        return !FieldValidator.notBlank(value) || /^[+-][0-9.,]+$/.test(value) && parseFloat(value) != NaN;
+        return !FieldValidator.notBlank(value) || /^[+-][0-9.,]+$/.test(value) && parseFloat(value.replace(/[^0-9.,]/g, "")) != NaN;
     }
 
 }
